46b8b686e57433859a3fc518e496d164fc6a5e7c
mosaabtalib <mosaab.talib09@gmail.com>
2021-06-11 20:54:45 +0800

    create manage account page

---
+++ src/pages/ManageAccount.vue
@@ -0,0 +1,14 @@
+<template>
+    <q-page class="bg-brown-1">
+       <div class="q-pa-md">
+       <q-table
+        title="Officers"
+        :data="data"
+        :columns="columns"
+        :filter="searchFilter"
+        row-key="name"
+      >
+       </q-table>
+      </div>
+    </q-page>
+</template>
