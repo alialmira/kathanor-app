62ddcdfdcc71e29cdd31ffd0366d12f16a0591f3
mosaabtalib <mosaab.talib09@gmail.com>
2021-06-11 20:59:36 +0800

    create home page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding class="bg-brown-1">
+    <div class="row no-wrap items-center q-mt-none q-pa-sm bg-white">
+      <q-avatar square size="xl">
+        <img height="100px" src="~assets/msuu.png" />
+      </q-avatar>
+      <div class="text-h6 q-mx-md">Hello</div>
+
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+.q-item {
+  height: 60px;
+}
+</style>
