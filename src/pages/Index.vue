ef9406e985ce75fad1abf9d9e57180fc5eea6712
mosaabtalib <mosaab.talib09@gmail.com>
2021-06-11 20:55:45 +0800

    create index page

---
+++ src/pages/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="flex flex-center bg-grey-12"> </q-page>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class PageIndex extends Vue {}
+</script>
