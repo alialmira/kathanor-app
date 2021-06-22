302705efd5ef259e08531768bfbb5d905e7baaca
mosaabtalib <mosaab.talib09@gmail.com>
2021-06-22 21:28:39 +0800

    at change password func at officer module

--- src/store/officer-module/mutations.ts
+++ src/store/officer-module/mutations.ts
@@ -4,6 +4,9 @@ import { OfficerStateInterface } from './state';
 const mutation: MutationTree<OfficerStateInterface> = {
   addOfficer(state, payload) {
     state.officers.push(payload);
+  },
+  changePassword(state, payload: { [key: string]: any }) {
+    state.officers[payload.index] = payload.newPassword;
   }
 };
 
