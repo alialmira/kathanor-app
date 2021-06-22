55d519f74f8fb73f2ad9ec2e0b5ca050ee40b49b
mosaabtalib <mosaab.talib09@gmail.com>
2021-06-22 21:30:17 +0800

    create add docs dialog component

---
+++ src/components/addDocsDialog.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-dialog v-model="addDoc" persistent>
+    <q-card class="__card q-py-lg">
+      <q-toolbar>
+        <q-avatar> <img src="~assets/icons8-add-document-48.png"/></q-avatar>
+        <q-toolbar-title class="text-weight-bold text-center">Add Document</q-toolbar-title>
+        <q-btn color="dark" icon="close" size="md" @click="docsPopup(false)">
+        </q-btn>
+      </q-toolbar>
+      <q-card-section class="q-gutter-y-md">
+        <q-input></q-input>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { mapState, mapActions } from 'vuex';
+
+@Component({
+  computed: {
+    ...mapState('uiNav', ['addDoc'])
+  },
+  methods: {
+    ...mapActions('uiNav', ['docsPopup'])
+  }
+})
+export default class addDocsDialog extends Vue {
+  addDoc!: boolean;
+  sshouldShow = false;
+  docsPopup!: (show: boolean) => void;
+
+  created() {
+    this.sshouldShow = this.addDoc;
+  }
+}
+</script>
+
+<style>
+.__card {
+  width: 500px;
+}
+</style>
