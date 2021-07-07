09e649c88904d3ef994554ed31ca6a1d49258d4e
ruzlicali16 <ruzlicali24@gmail.com>
2021-07-07 20:32:07 +0800

    setup sms service

--- src/services/sms.service.ts
+++ src/services/sms.service.ts
@@ -1,11 +1,29 @@
-import { DocumentControllerApi } from './rest-api/api';
+import { DocumentControllerApi, Configuration, Documents } from './rest-api';
+
+const dev = 'http://localhost:9000';
+
+const restConfig = new Configuration({
+  basePath: dev,
+  baseOptions: {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
+});
+
+const restApi = new DocumentControllerApi(restConfig);
 
 class SmsService extends DocumentControllerApi {
   async getAll() {
-    const response = await this.getDocuments();
-    console.log(response.data);
+    const response = await restApi.getDocuments();
+    return response.data;
+  }
+
+  async insert(payload: Documents) {
+    const response = await restApi.addDocument(payload);
+    return response.data;
   }
 }
 
-let smsService = new SmsService();
+const smsService = new SmsService();
 export default smsService;
