883a1ae5de0eee7b694477fe9968821db7f404e5
ruzlicali16 <ruzlicali24@gmail.com>
2021-07-11 14:59:42 +0800

    update action + mutation + state at recipient store module

--- src/store/recipient-module/state.ts
+++ src/store/recipient-module/state.ts
@@ -1,53 +1,14 @@
+import IRecipient from 'src/interfaces/recipient.interface';
+
 export interface RecipientStateInterface {
-  recipients: {
-    name: string;
-    fName: string;
-    lName: string;
-    offCollege: string;
-    status: string;
-  }[];
-  newRecipients: {
-    name: string;
-    fName: string;
-    lName: string;
-    offCollege: string;
-    status: string;
-  }[];
+  recipients: IRecipient[];
+  newRecipients: IRecipient[];
   institution: string[];
 }
 
 function state(): RecipientStateInterface {
   return {
-    recipients: [
-      {
-        name: '09077435078',
-        fName: 'Mohamed Rusli',
-        lName: 'Cali',
-        offCollege: 'CIT',
-        status: 'Student'
-      },
-      {
-        name: '09091233245',
-        fName: 'Mosaab',
-        lName: 'Talib',
-        offCollege: 'CBAA',
-        status: 'Student'
-      },
-      {
-        name: '09658452491',
-        fName: 'Omair',
-        lName: 'Mangondaya',
-        offCollege: 'UBO',
-        status: 'Staff'
-      },
-      {
-        name: '09965815436',
-        fName: 'Abdul Matin',
-        lName: 'Maruhom',
-        offCollege: 'COE',
-        status: 'Faculty'
-      }
-    ],
+    recipients: [],
     newRecipients: [],
     institution: []
   };
