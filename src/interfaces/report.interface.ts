44e994874dc7c417da8ecde412cd2476b21fc91b
Almira <almiramarilanali@gmail.com>
2022-09-10 20:12:00 +0600

    added report interface

---
+++ src/interfaces/report.interface.ts
@@ -0,0 +1,9 @@
+export default interface IEmployeeReport {
+  fullname: string;
+  birthDate: string;
+  accountType?: string;
+  dateRecorded: string;
+  day: string;
+  month: string;
+  year: string;
+}
